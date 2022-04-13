96b02dd9f286202bdb3f99509721744a07dde5e1
MidasDev <johnmaeda913@gmail.com>
2022-04-14 06:52:22 +0800

    [chore] modify css and rm totlal in dashboard (#7)

--- src/components/LoadScript/index.tsx
+++ src/components/LoadScript/index.tsx
@@ -5,37 +5,33 @@ const LoadScript = () => {
   return (
     <>
       <Script
-      strategy="beforeInteractive"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{ __html: `function __googleMapsCallback() {}` }} />
+      <Script
+        strategy="beforeInteractive"
         type="text/javascript"
         src="https://maps.googleapis.com/maps/api/js?callback=__googleMapsCallback&amp;key=AIzaSyABjeZnSnoEubAX5H6YgelDYVsPmyOi2Ng&amp;libraries=places&amp;v=weekly&amp;language=en"
       ></Script>
 
       <Script
-      strategy="beforeInteractive"
+        strategy="beforeInteractive"
         async
         src="https://www.googletagmanager.com/gtag/js?id=AW-10924165313"
       ></Script>
-      <Script>
-        {/* window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', 'AW-10924165313'); */}
-      </Script>
       <Script
-      strategy="beforeInteractive"
-      dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || [];
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
             function gtag() {
               dataLayer.push(arguments);
               gtag('js', new Date());
         gtag('config', 'AW-10924165313');` }} />
       <Script
-      strategy="beforeInteractive"
-      src="https://unpkg.com/pdfjs-dist/build/pdf.min.js"></Script>
+        strategy="beforeInteractive"
+        src="https://unpkg.com/pdfjs-dist/build/pdf.min.js"></Script>
       <Script
-      strategy="beforeInteractive"
-      src="https://unpkg.com/pdfjs-dist/build/pdf.worker.min.js"></Script>
+        strategy="beforeInteractive"
+        src="https://unpkg.com/pdfjs-dist/build/pdf.worker.min.js"></Script>
     </>
   )
 }
