fdf6fe75f973e0817b154ea060eb93fab6ff9b10
MidasDev <johnmaeda913@gmail.com>
2022-04-14 06:08:02 +0800

    Web3 jobs 466 post a job location (#6)
    * [chore] removed env settings from next.config.js
    
    * [fix] added loadscript component & fix location suggession error

---
+++ src/components/LoadScript/index.tsx
@@ -0,0 +1,43 @@
+import Script from 'next/script'
+
+const LoadScript = () => {
+
+  return (
+    <>
+      <Script
+      strategy="beforeInteractive"
+        type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?callback=__googleMapsCallback&amp;key=AIzaSyABjeZnSnoEubAX5H6YgelDYVsPmyOi2Ng&amp;libraries=places&amp;v=weekly&amp;language=en"
+      ></Script>
+
+      <Script
+      strategy="beforeInteractive"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-10924165313"
+      ></Script>
+      <Script>
+        {/* window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'AW-10924165313'); */}
+      </Script>
+      <Script
+      strategy="beforeInteractive"
+      dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+              gtag('js', new Date());
+        gtag('config', 'AW-10924165313');` }} />
+      <Script
+      strategy="beforeInteractive"
+      src="https://unpkg.com/pdfjs-dist/build/pdf.min.js"></Script>
+      <Script
+      strategy="beforeInteractive"
+      src="https://unpkg.com/pdfjs-dist/build/pdf.worker.min.js"></Script>
+    </>
+  )
+}
+
+export default LoadScript;
